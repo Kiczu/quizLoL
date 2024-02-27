7419e8f6cf38de4637f39efad33d52fc6f3894cb
Kiczu <adrian.mamyto@gmail.com>
2024-02-27 21:33:04 +0100

    Add api file, react-router and HomeRoute in Layout component

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Hello World</div>} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default Layout;
