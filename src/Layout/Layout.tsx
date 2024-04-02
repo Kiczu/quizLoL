f6d5c3d855a8ccb81c64cbab8d5ac8f9c7728bbe
Kiczu <adrian.mamyto@gmail.com>
2024-04-02 16:59:47 +0200

    Add init

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { getJinx } from "../api/firebase/firebse";
+import Home from "../views/Home/Home";
+
+getJinx();
 
 const Layout = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/api/firebase/firebse.ts
@@ -0,0 +1,30 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, collection, getDocs } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_6SHqYUNm-11WYr9U2vxrWYTj0AF7Rws",
+    authDomain: "quiz-lol-21e54.firebaseapp.com",
+    projectId: "quiz-lol-21e54",
+    storageBucket: "quiz-lol-21e54.appspot.com",
+    messagingSenderId: "85626427765",
+    appId: "1:85626427765:web:1bb11a9cae9e270c13db3c"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth();
+
+export const getJinx = async () => {
+    const ref = doc(db, 'champions', 'mQTzrtYjpdVEZDZxWmQ6');
+    const champion = await getDoc(ref);
+
+    const ref2 = collection(db, 'champions');
+    const champions = await getDocs(ref2);
+
+    //  console.log(champion.data());
+
+    champions.forEach(el => {
+        console.log(el.data());
+    })
+} 
