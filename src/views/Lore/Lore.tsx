b613eba1c0ce2a16792e32019e8b42217f9d0461
Kiczu <adrian.mamyto@gmail.com>
2024-06-06 07:07:33 +0200

    Add MUI components

--- src/views/Lore/Lore.tsx
+++ src/views/Lore/Lore.tsx
@@ -1,6 +1,7 @@
 import { characterService } from "../../api/characterService";
 import { useEffect, useState } from "react";
 import { Character } from "../../api/types";
+import { Grid, Card } from "@mui/material";
 
 const Lore = () => {
   const [champions, setChampions] = useState<Character[]>([]);
@@ -22,15 +23,21 @@ const Lore = () => {
     <div style={{ padding: "100px 20px" }}>
       <h1>LORE</h1>
       <div>
-        {champions.map((champion) => (
-          <div key={champion.id}>
-            <img
-              src={champion.image.full}
-              alt={champion.name}
-            />
-            <h2>{champion.name}</h2>
-          </div>
-        ))}
+        <Grid container spacing={2}>
+          {champions.map((champion) => (
+            <Grid item xs={12} md={3} key={champion.id}>
+              <Card className="card">
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`}
+                  alt={champion.name}
+                  loading="lazy"
+                />
+                <h2>{champion.name}</h2>
+                <p>{champion.blurb}</p>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </div>
   );
