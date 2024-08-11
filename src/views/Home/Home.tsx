0cc4ab6c5a0a1ad581c0b38f6e8547e04971937d
Kiczu <adrian.mamyto@gmail.com>
2024-08-11 15:51:12 +0200

    Ref: Drawing out styles in Home component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,7 @@
 import { Box, Typography } from "@mui/material";
-import SouthIcon from "@mui/icons-material/South";
+import { heroImageContainer, overlayHeroImage } from "./heroImageStyles";
 import { useAuth } from "../../context/LoginContext/LoginContext";
 import logo from "../../assets/images/logo.png";
-import heroImage from "../../assets/images/hero.jpg";
-import { Link } from "react-router-dom";
 import SelectModeCard from "../../components/SelectModeCard/SelectModeCard";
 
 const Home = () => {
@@ -11,27 +9,8 @@ const Home = () => {
 
   return (
     <>
-      <Box
-        sx={{
-          position: "relative",
-          backgroundImage: `url(${heroImage})`,
-          backgroundPosition: "top center",
-          backgroundSize: "cover",
-          height: "100vh",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-            background: "linear-gradient(180deg, rgba(0,0,0,0), black);",
-          }}
-        >
+      <Box sx={heroImageContainer}>
+        <Box sx={overlayHeroImage}>
           <Box
             component="img"
             alt="Logo"
