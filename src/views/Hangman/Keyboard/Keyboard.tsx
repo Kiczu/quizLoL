4041e4eab51d7dc03e7c7d445a2f49decae4b97a
Kiczu <adrian.mamyto@gmail.com>
2024-09-24 18:53:49 +0200

    style: change navi color and keyboard buttons

--- src/views/Hangman/Keyboard/Keyboard.tsx
+++ src/views/Hangman/Keyboard/Keyboard.tsx
@@ -1,27 +1,33 @@
 import { Button, Grid } from "@mui/material";
 
-const Keyboard = ({userGuess}: any) => {
-    return (
-        <Grid container spacing={1} justifyContent="center">
-        {Array.from({ length: 26 }, (_, i) => (
-          <Grid item key={i}>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={() => userGuess(String.fromCharCode(65 + i))}
-              sx={{
-                minWidth: "40px",
-                minHeight: "40px",
-                fontSize: "1rem",
-                textTransform: "none",
-              }}
-            >
-              {String.fromCharCode(65 + i)}
-            </Button>
-          </Grid>
-        ))}
-      </Grid>
-    )
-}
+const Keyboard = ({ userGuess }: any) => {
+  return (
+    <Grid container spacing={1} justifyContent="center">
+      {Array.from({ length: 26 }, (_, i) => (
+        <Grid item key={i}>
+          <Button
+            variant="outlined"
+            onClick={() => userGuess(String.fromCharCode(65 + i))}
+            sx={{
+              color: "#F0E6D2",
+              borderColor: "#C8AA6E",
+              minWidth: "40px",
+              minHeight: "40px",
+              fontSize: "1rem",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "#C8AA6E",
+                borderColor: "#C8AA6E",
+                color: "#32281E",
+              },
+            }}
+          >
+            {String.fromCharCode(65 + i)}
+          </Button>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export default Keyboard;
