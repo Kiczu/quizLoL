1d01beff9d0a8cd35fbe3d760a98f9e7bc39f178
Kiczu <adrian.mamyto@gmail.com>
2024-09-29 21:12:58 +0200

    refactor: change styles, fixed errors height, change from Polish to English

--- src/views/RegisterPage/RegisterPage.tsx
+++ src/views/RegisterPage/RegisterPage.tsx
@@ -2,18 +2,19 @@ import * as yup from "yup";
 import {
   Avatar,
   Button,
-  CssBaseline,
   TextField,
   Typography,
   Container,
   Link,
   Grid,
   Box,
+  CssBaseline,
 } from "@mui/material";
 import { Form, Formik } from "formik";
 import { useAuth } from "../../context/LoginContext/LoginContext";
 import { paths } from "../../paths";
 import type { UserData } from "../../api/types";
+import { FormContainer } from "./register.style";
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("Imię jest wymagane"),
@@ -53,141 +54,149 @@ const RegisterPage = () => {
   const handleSubmit = (values: UserData) => handleCreateUser(values);
 
   return (
-    <>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 15,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
+    <Container component="div" maxWidth="xs">
+      <CssBaseline enableColorScheme />
+      <Box sx={FormContainer}>
+        <Avatar sx={{ m: 1 }}></Avatar>
+        <Typography component="h1" variant="h5" margin={2}>
+          Sign up
+        </Typography>
+        <Formik
+          initialValues={initValues}
+          onSubmit={handleSubmit}
+          validationSchema={registerSchema}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-          <Typography component="h1" variant="h5" margin={2}>
-            Zarejestruj się
-          </Typography>
-          <Formik
-            initialValues={initValues}
-            onSubmit={handleSubmit}
-            validationSchema={registerSchema}
-          >
-            {({
-              values,
-              handleBlur,
-              handleChange,
-              handleSubmit,
-              errors,
-              touched,
-            }) => (
-              <Form onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      autoComplete="given-name"
-                      name="firstName"
-                      fullWidth
-                      id="firstName"
-                      label="Imię"
-                      autoFocus
-                      value={values.firstName}
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      error={
-                        Boolean(touched.firstName) && Boolean(errors.firstName)
-                      }
-                      helperText={touched.firstName && errors.firstName}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      autoComplete="family-name"
-                      name="lastName"
-                      fullWidth
-                      id="lastName"
-                      label="Nazwisko"
-                      value={values.lastName}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={
-                        Boolean(touched.lastName) && Boolean(errors.lastName)
-                      }
-                      helperText={touched.lastName && errors.lastName}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      autoComplete="email"
-                      name="email"
-                      fullWidth
-                      id="email"
-                      label="Adres E-mail"
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={Boolean(touched.email) && Boolean(errors.email)}
-                      helperText={touched.email && errors.email}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      name="password"
-                      label="Hasło"
-                      type="password"
-                      id="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={
-                        Boolean(touched.password) && Boolean(errors.password)
-                      }
-                      helperText={touched.password && errors.password}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      name="confirmPassword"
-                      label="Potwierdź hasło"
-                      type="password"
-                      id="confirmPassword"
-                      value={values.confirmPassword}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={
-                        Boolean(touched.confirmPassword) &&
-                        Boolean(errors.confirmPassword)
-                      }
-                      helperText={
-                        touched.confirmPassword && errors.confirmPassword
-                      }
-                    />
-                  </Grid>
+          {({
+            values,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            errors,
+            touched,
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    fullWidth
+                    id="firstName"
+                    label="First name"
+                    autoFocus
+                    value={values.firstName}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={
+                      Boolean(touched.firstName) && Boolean(errors.firstName)
+                    }
+                    helperText={
+                      touched.firstName && errors.firstName
+                        ? errors.firstName
+                        : " "
+                    }
+                  />
                 </Grid>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Zarejestruj
-                </Button>
-                <Grid container justifyContent="flex-end">
-                  <Grid item>
-                    <Link href={paths.LOGIN} variant="body2">
-                      Masz już konto? Zaloguj się
-                    </Link>
-                  </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="family-name"
+                    name="lastName"
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={
+                      Boolean(touched.lastName) && Boolean(errors.lastName)
+                    }
+                    helperText={
+                      touched.lastName && errors.lastName
+                        ? errors.lastName
+                        : " "
+                    }
+                  />
                 </Grid>
-              </Form>
-            )}
-          </Formik>
-        </Box>
-      </Container>
-    </>
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="email"
+                    name="email"
+                    fullWidth
+                    id="email"
+                    label="Email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={Boolean(touched.email) && Boolean(errors.email)}
+                    helperText={
+                      touched.email && errors.email ? errors.email : " "
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={
+                      Boolean(touched.password) && Boolean(errors.password)
+                    }
+                    helperText={
+                      touched.password && errors.password
+                        ? errors.password
+                        : " "
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    type="password"
+                    id="confirmPassword"
+                    value={values.confirmPassword}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={
+                      Boolean(touched.confirmPassword) &&
+                      Boolean(errors.confirmPassword)
+                    }
+                    helperText={
+                      touched.confirmPassword && errors.confirmPassword
+                        ? errors.confirmPassword
+                        : " "
+                    }
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                size="large"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href={paths.LOGIN} variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </Container>
   );
 };
 
