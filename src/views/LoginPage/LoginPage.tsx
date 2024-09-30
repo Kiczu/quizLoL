8abe12d2f4327457469adaf21f197c2098343f50
Kiczu <adrian.mamyto@gmail.com>
2024-09-30 10:44:54 +0200

    styles: changing the style of the google login button

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -10,21 +10,10 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import { styled } from "@mui/material/styles";
 import { Form, Formik } from "formik";
 import { useAuth } from "../../context/LoginContext/LoginContext";
 import { paths } from "../../paths";
 
-const LoginWithGooglebutton = styled(Button)({
-  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-  border: 0,
-  borderRadius: 3,
-  boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  color: "white",
-  height: 48,
-  padding: "0 30px",
-  width: "100%",
-});
 const loginSchema = yup.object().shape({
   email: yup
     .string()
@@ -154,9 +143,12 @@ const LoginPage = () => {
                       Or login with:
                     </Box>
                     <Grid justifyContent="center">
-                      <LoginWithGooglebutton onClick={handleSignInWithGoogle}>
+                      <Button
+                        variant="outlined"
+                        onClick={handleSignInWithGoogle}
+                      >
                         Google
-                      </LoginWithGooglebutton>
+                      </Button>
                     </Grid>
                   </Box>
                 </Form>
