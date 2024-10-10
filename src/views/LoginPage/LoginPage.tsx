21a7fbc34b6983726bf4d31a1b1a2e9f38f597fb
Kiczu <adrian.mamyto@gmail.com>
2024-10-10 10:56:26 +0200

    styles: refactor styles for loginPage

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -2,7 +2,6 @@ import * as yup from "yup";
 import {
   Avatar,
   Button,
-  CssBaseline,
   TextField,
   Link,
   Paper,
@@ -42,125 +41,121 @@ const LoginPage = () => {
 
   return (
     <>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: "90vh",
+          backgroundImage: `url(${getRandomImage()})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Grid item xs={false} sm={4} md={7} />
         <Grid
           item
-          xs={false}
-          sm={4}
-          md={7}
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          square
           sx={{
-            backgroundImage: `url(${getRandomImage()})`,
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backdropFilter: "blur(4px)",
+            backgroundColor: "rgba(10, 20, 40 ,0.6)",
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Formik
+            initialValues={initValues}
+            onSubmit={handleSubmit}
+            validationSchema={loginSchema}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Formik
-              initialValues={initValues}
-              onSubmit={handleSubmit}
-              validationSchema={loginSchema}
-            >
-              {({
-                values,
-                handleChange,
-                handleSubmit,
-                handleBlur,
-                errors,
-                touched,
-              }) => (
-                <Form onSubmit={handleSubmit}>
-                  <Box sx={{ mt: 1 }}>
-                    <TextField
-                      margin="normal"
-                      fullWidth
-                      id="email"
-                      label="E-Mail"
-                      name="email"
-                      autoComplete="email"
-                      autoFocus
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={Boolean(touched.email) && Boolean(errors.email)}
-                      helperText={
-                        touched.email && errors.email ? errors.email : " "
-                      }
-                    />
-                    <TextField
-                      margin="normal"
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      autoComplete="current-password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={
-                        Boolean(touched.password) && Boolean(errors.password)
-                      }
-                      helperText={
-                        touched.password && errors.password
-                          ? errors.password
-                          : " "
-                      }
-                    />
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      sx={{ mb: 2 }}
-                    >
-                      Sign In
-                    </Button>
-                    <Grid container>
-                      <Grid item xs>
-                        <Link href={paths.RESET_PASSWORD} variant="body2">
-                          Forgot password?
-                        </Link>
-                      </Grid>
-                      <Grid item>
-                        <Link href={paths.REGISTER} variant="body2">
-                          Don't have an account? Sign Up
-                        </Link>
-                      </Grid>
+            {({
+              values,
+              handleChange,
+              handleSubmit,
+              handleBlur,
+              errors,
+              touched,
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Box sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    id="email"
+                    label="E-Mail"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={Boolean(touched.email) && Boolean(errors.email)}
+                    helperText={
+                      touched.email && errors.email ? errors.email : " "
+                    }
+                  />
+                  <TextField
+                    margin="normal"
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={
+                      Boolean(touched.password) && Boolean(errors.password)
+                    }
+                    helperText={
+                      touched.password && errors.password
+                        ? errors.password
+                        : " "
+                    }
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mb: 2 }}
+                  >
+                    Sign In
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link href={paths.RESET_PASSWORD} variant="body2">
+                        Forgot password?
+                      </Link>
                     </Grid>
-                    <Box m={2} mx={0}>
-                      Or login with:
-                    </Box>
-                    <Grid justifyContent="center">
-                      <Button
-                        variant="outlined"
-                        onClick={handleSignInWithGoogle}
-                      >
-                        Google
-                      </Button>
+                    <Grid item>
+                      <Link href={paths.REGISTER} variant="body2">
+                        Don't have an account? Sign Up
+                      </Link>
                     </Grid>
+                  </Grid>
+                  <Box m={2} mx={0}>
+                    Or login with:
                   </Box>
-                </Form>
-              )}
-            </Formik>
-          </Box>
+                  <Grid justifyContent="center">
+                    <Button variant="outlined" onClick={handleSignInWithGoogle}>
+                      Google
+                    </Button>
+                  </Grid>
+                </Box>
+              </Form>
+            )}
+          </Formik>
         </Grid>
       </Grid>
     </>
